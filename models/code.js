d138f0cebc1727cd91b44a85f8f877678b438d73
sedat sevgili <sedatsevgili@gmail.com>
2016-09-09 18:17:24 +0300

    created oauth2 code model

---
+++ models/code.js
@@ -0,0 +1,23 @@
+var restful = require('node-restful');
+var mongoose = restful.mongoose;
+
+var CodeSchema = new mongoose.Schema({
+  value: {
+    type: String,
+    required: true
+  },
+  redirectUri: {
+    type: String,
+    required: true
+  },
+  userId: {
+    type: String,
+    required: true
+  },
+  clientId: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Code', CodeSchema);
