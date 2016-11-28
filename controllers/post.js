feea0d2105f0a5b110f34bf5e66076fe98e871fd
sedat sevgili <sedatsevgili@gmail.com>
2016-11-28 19:03:43 +0300

    implemented sorting, searching and filtering

--- controllers/post.js
+++ controllers/post.js
@@ -7,7 +7,9 @@ exports.postPosts = function(req, res) {
   post.content = req.body.content;
   post.userId = req.user._id;
   post.createdAt = new Date();
-  post.hidden = req.body.hidden;
+  if(req.body.hidden) {
+    post.hidden = req.body.hidden;
+  }
 
   TagBinder.bindTags(post, req.body.tags, function(err, post) {
     if(err) {
@@ -26,7 +28,39 @@ exports.postPosts = function(req, res) {
 }
 
 exports.getPosts = function(req, res) {
-  Post.find({userId: req.user._id}, function(err, posts) {
+  var query = {userId: req.user._id, hidden: false};
+  if(req.query.q) {
+    query.content = new RegExp(req.query.q, 'i');
+  }
+  if(req.query.filter) {
+    query.tags = {
+      $elemMatch: {name: req.query.filter}
+    };
+  }
+  var sortingOptions = {
+    skip: 0,
+    limit: 10,
+    sort: {
+      createdAt: -1
+    }
+  };
+  if(req.query.skip) {
+    sortingOptions.skip = parseInt(req.query.skip);
+  }
+  if(req.query.limit) {
+    sortingOptions.limit = parseInt(req.query.limit);
+  }
+  if(req.query.sort && (typeof req.query.sort) == 'object') {
+    sortingOptions.sort = {};
+    var sortMatcher = {'asc' : '1', 'desc' : '-1'};
+    for (var key in req.query.sort) {
+      var value = req.query.sort[key];
+      if(value in sortMatcher) {
+        sortingOptions.sort[key] = sortMatcher[value];
+      }
+    }
+  }
+  Post.find(query, null, sortingOptions, function(err, posts) {
     if(err) {
       return res.send(err);
     }
